3ece2a158e1e49a9751b9278f405d1dd61ce3d88
Sofian Benaissa <me@sfyn.net>
2020-03-23 19:46:29 -0400

    Always show nav bar on refresh

--- js/sticky-nav.js
+++ js/sticky-nav.js
@@ -1,10 +1,14 @@
+function stickit() {
+    if ($(window).scrollTop() > 10) {
+	$('header').addClass('sticky');
+    } else {
+	$('header').removeClass('sticky');
+    }
+}
 
 $(document).ready(function() {
+	stickit();
 	$(window).bind('scroll', function () {
-	    if ($(window).scrollTop() > 10) {
-		$('header').addClass('sticky');
-	    } else {
-		$('header').removeClass('sticky');
-	    }
+		stickit();
 	});
 });
