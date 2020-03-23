65d2a3a5db4b44d96dd8625507fbd6a9dfac5e59
Sofian Benaissa <me@sfyn.net>
2020-03-23 19:43:21 -0400

    preserve section link color on refresh

--- js/current-link.js
+++ js/current-link.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+	if (window.location.hash) {
+		$("body header nav a[href='"+window.location.hash+"'").addClass("current");
+	}
 	$("body header nav a[href^='#'").click(function(e) {
 		$(this).addClass("current");
 		$(this).siblings('a').removeClass("current");
